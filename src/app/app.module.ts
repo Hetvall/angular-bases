14f5b26c3b3550e613f9f4c4ff75548723e528ac
James Orozco <jamesorozcoh@gmail.com>
2024-08-08 23:50:58 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 
 import { CounterModule } from './counter/counter.module';
+import { DbzModule } from './dbz/dbz.module';
 import { HeroesModules } from './heroes/heroes.module';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CounterModule, HeroesModules],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CounterModule,
+    HeroesModules,
+    DbzModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
